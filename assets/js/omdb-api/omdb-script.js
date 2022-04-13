2372eda8ea9658fcf7746f6d657ce145c8e8a63e
Sfzmango <maunghtike1999@gmail.com>
2022-04-12 21:43:22 -0700

    add list of movies

--- assets/js/omdb-api/omdb-script.js
+++ assets/js/omdb-api/omdb-script.js
@@ -1,1 +1,20 @@
-console.log("testing branch");
+//7b7875f8
+//c1760db1a7fd46be9a1c44d5afcd8020
+
+var romanceArr = ["It's a Wonderful Life", "Casablanca", "Your Name.", "Eternal Sunshine of the Spotless Mind", "Gone with the Wind", "Howl's Moving Castle", "Her", "The Notebook", "The Fault in Our Stars", "Midnight in Paris", "500 Days of Summer", "Love, Simon", "A Star Is Born", "50 First Dates", "La La Land", "Titanic", "Edward Scissorhands", "When a Man Loves a Woman", "Kate & Leopold", "The Perks of Being a Wallflower"];
+var horrorArr = ["Alien", "The Shining", "2001: A Space Odyssey", "The Thing", "The Exorcist", "Dawn of the Dead", "Night of the Living Dead", "Get Out", "The Evil Dead", "Halloween", "Split", "A Quiet Place", "Midsommar", "The Ring", "Cloverfield"];
+var thrillerArr = ["Inception", "Se7en", "The Silence of the Lambs", "Parasite", "The Departed", "Memento", "Joker", "Reservoir Dogs", "Shutter Island", "No Country for Old Men", "Fargo", "Gone Girl", "V for Vendetta", "Jaws", "The Sixth Sense"];
+var comedyArr = ["Back to the Future", "Superbad", "Spider-Man: Into the Spider-Verse", "Coco", "The Wolf of Wall Street", "Monty Python and the Holy Grail", "The Truman Show", "The 40-Year-Old Virgin", "The Big Lebowski", "Catch Me If You Can", "Shaun of the Dead", "Shrek", "Ratatouille", "Life of Brian", "What We Do in the Shadows"];
+var actionArr = ["The Dark Knight", "The Good, the Bad and the Ugly", "The Matrix", "The Terminator", "Gladiator", "Django Unchained", "Inglourious Basterds", "The Batman", "Kill Bill: Vol. 1", "Blade Runner", "Hacksaw Ridge", "Mad Max: Fury Road", "Logan", "Guardians of the Galaxy", "Deadpool"];
+var dramaArr = ["The Shawshank Redemption", "The Godfather", "Pulp Fiction", "Forrest Gump", "Fight Club", "Goodfellas", "The Green Mile", "The Usual Suspects", "The Prestige", "Whiplash", "Grave of the Fireflies", "Apocalypse Now", "Braveheart", "Once Upon a Time in America", "Taxi Driver",];
+var adventureArr = ["Interstellar", "The Lord of the Rings: The Fellowship of the Ring", "Star Wars", "Spirited Away", "The Lion King", "Indiana Jones and the Raiders of the Lost Ark", "WALL·E", "Princess Mononoke", "Stand by Me", "The Wizard of Oz", "Pirates of the Caribbean: The Curse of the Black Pearl", "Cast Away", "The Iron Giant", "Big Hero 6", "Harry Potter and the Sorcerer's Stone"]
+var allArr = actionArr.concat(romanceArr, horrorArr, thrillerArr, comedyArr, actionArr, dramaArr, adventureArr);
+
+console.log(romanceArr);
+console.log(horrorArr);
+console.log(thrillerArr);
+console.log(comedyArr);
+console.log(actionArr);
+console.log(dramaArr);
+console.log(adventureArr);
+console.log(allArr);
