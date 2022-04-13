9d084a7964369c73a0accb3b2db508f78d667ad8
Sfzmango <maunghtike1999@gmail.com>
2022-04-13 11:08:16 -0700

    add function to get another random drink on btn

--- assets/js/cocktail-api/cocktail-script.js
+++ assets/js/cocktail-api/cocktail-script.js
@@ -1,18 +1,33 @@
 var cocktailURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php?"
 
-fetch(cocktailURL)
-    .then(function (res) {
-        return res.json();
-    })
-    .then(function (data) {
-        console.log("Cocktail Data: ", data.drinks[0]);
-        var randomDrink = data.drinks[0];
+$("#cocktails").append().html(
+    `<button id="cocktailsBtn">Test</button>`
+);
 
-        $("#cocktails").prepend().html(
-            `<div id="cocktail">
+function randomDrink() {
+    fetch(cocktailURL)
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            console.log("Cocktail Data: ", data.drinks[0]);
+            var randomDrink = data.drinks[0];
+
+            $("#cocktails").append().html(
+                `<div id="cocktail">
                 <h4>${randomDrink.strDrink}</h4>
                 <h5>${randomDrink.strAlcoholic}</h5>
                 <h5><a href="https://www.youtube.com/results?search_query=how+to+make+${randomDrink.strDrink}+drink">How to make</a></h5>
-                <img class="cocktailImg" width="35%" src= "${randomDrink.strDrinkThumb}"/>
+                <img class="cocktailImg" width="35%" src= "${randomDrink.strDrinkThumb}"/> 
+                <button onclick="randomDrink()">Test</button>
             </div>`);
-    })
+        });
+};
+
+// adds click listener to execute movie generation
+$("#cocktailsBtn").on("click", function (event) {
+    event.preventDefault();
+
+    randomDrink();
+});
+
