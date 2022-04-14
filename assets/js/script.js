283b5925541a39a009b51807c5d5c6a11f861a06
M A Razvi <abuzarflw@gmail.com>
2022-04-14 02:48:11 -0700

    Add functionality to form and buttons on movie card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,35 @@
-function displayResults() {
+//Display movie card
+$("#startBtn").on("click", function (event) {
+    event.preventDefault();
+    
+    displayMovieBox();
+});
+
+function displayMovieBox() {
     $("#firstPage").css("display", "none")
     $("footer").css("display", "none");
     $("#movies").css("display", "block")
 }
-$("#startBtn").on("click", function (event) {
-    event.preventDefault();
 
-    displayResults();
-});
+//Display food card
+$("#genres").on("change", function() {
+    $("#movieBtn").css("pointerEvents", "auto");
+})
+
+$("#movieBtn").on("click", function() {
+    $("#saveMovieBtn").css("pointerEvents", "auto");
+})
+
+
+$("#saveMovieBtn").on("click", function(event) {
+    event.preventDefault();
+    console.log("clickity");
+    displayFoodBox();
+})
+
+function displayFoodBox() {
+    $("#movies").css("display", "none")
+    $("#dinner").css("display", "block");
+
+}
+
